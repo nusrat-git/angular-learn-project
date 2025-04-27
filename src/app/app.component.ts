d438124f2801af256c50f6bd3dd5fffb2c7dc9b1
Nusrat Jahan <nusratjahanmithila66@gmail.com>
2025-04-27 21:02:13 +0600

    navbar style added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { provideHttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from './components/shared/topbar/topbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterOutlet, TopbarComponent, TopbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
